0168a1fadef34431250afde16ebc8bff43953930
gsingh-collab <gsingh@eightfold.ai>
2026-04-23 12:57:56 +0530

    Github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/overview-page/' : '/',
   plugins: [react()],
   server: {
-    port: 5173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : undefined,
     host: true,
   },
 });
